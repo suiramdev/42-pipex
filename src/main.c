32695b5a35cb81042416a07adc99d2fc21873914
suiramdev <mariusnouchet@gmail.com>
2023-02-05 19:57:20 +0100

    :sparkles: Can now execute piped commands, with an inputFile

--- src/main.c
+++ src/main.c
@@ -6,31 +6,89 @@
 /*   By: mnouchet <mnouchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 19:25:06 by mnouchet          #+#    #+#             */
-/*   Updated: 2023/02/04 19:18:36 by mnouchet         ###   ########.fr       */
+/*   Updated: 2023/02/05 19:55:07 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "types/commands.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
 #include <libft.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main()
+static void	close_pipes(int **fd)
 {
-	pid_t pid;
+	int	i;
 
-	printf("Start fork\n");
-	pid = fork();
-	if (pid < 0)
-		return (EXIT_FAILURE);
-	if (pid == 0)
+	i = 0;
+	while (fd[i])
 	{
-		printf("PID %d\n", pid);
-		return (EXIT_SUCCESS);
+		close(fd[i][0]);
+		close(fd[i][1]);
+		free(fd[i]);
+		i++;
 	}
-	printf("PID %d\n", pid);
-	printf("End fork\n");
+	free(fd);
+}
+
+static int	**setup_pipes(int count)
+{
+	int	**fd;
+	int	i;
+
+	fd = malloc(sizeof(int *) * (count) + 1);
+	if (!fd)
+		return (NULL);
+	i = 0;
+	while (i < count)
+	{
+		fd[i] = malloc(sizeof(int) * 2);
+		if (!fd[i] || pipe(fd[i]) < 0)
+		{
+			close_pipes(fd);
+			return (NULL);
+		}
+		i++;
+	}
+	fd[i] = NULL;
+	return (fd);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int **fd;
+	int i;
+	pid_t pid;
+	t_command cmd;
+	char buffer[400];
+
+	if (argc < 4)
+		return (EXIT_FAILURE);
+	fd = setup_pipes(argc - 2);
+	if (!fd)
+		return (EXIT_FAILURE);
+	dup2(open(argv[1], O_RDONLY), STDIN_FILENO);
+	i = 0;
+	while (i < argc - 3)
+	{
+		pid = fork();
+		if (pid < 0)
+			return (EXIT_FAILURE);
+		if (pid == 0)
+		{
+			dup2(fd[i][1], STDOUT_FILENO);
+			cmd = parse_command(argv[i + 2]);
+			execve(cmd.file, cmd.args, envp);
+			return (EXIT_FAILURE);
+		}
+		wait(&pid);
+		close(fd[i][1]);
+		dup2(fd[i][0], STDIN_FILENO);
+		i++;
+	}
+	while (read(fd[i - 1][0], buffer, 400) > 0)
+		printf("%s", buffer);
+	close_pipes(fd);
 	return (EXIT_SUCCESS);
 }
--- src/types/commands.c
+++ src/types/commands.c
@@ -6,7 +6,7 @@
 /*   By: mnouchet <mnouchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 23:08:20 by mnouchet          #+#    #+#             */
-/*   Updated: 2023/01/31 18:05:05 by mnouchet         ###   ########.fr       */
+/*   Updated: 2023/02/05 00:37:58 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,8 @@ static char	*join_path(char *path, char *file)
 }
 
 /**
- * This function retrieve the given file's path, by looking to the current directory and the PATH env.
+ * This function retrieve the given file's path,
+	by looking to the current directory and the PATH env.
  *
  * @param file
  */
@@ -41,21 +42,26 @@ static char	*resolve_file(char *file)
 {
 	char	*path;
 	char	**env;
+	size_t	i;
 
 	if (access(file, X_OK) >= 0)
 		return (file);
-	path = NULL;
 	env = ft_env("PATH");
+	i = 0;
+	while (env && env[i])
+	{
+		path = join_path(env[i], file);
+		if (access(path, X_OK) >= 0)
+			break ;
+		free(path);
+		i++;
+	}
 	while (env && *env)
 	{
-		if (!path) 
-		{
-			path = join_path(*env, file);
-			if (access(path, X_OK) < 0)
-				free(path);
-		}
-		free(env++);
+		free(*env);
+		env++;
 	}
+	free(env);
 	return (path);
 }
 
